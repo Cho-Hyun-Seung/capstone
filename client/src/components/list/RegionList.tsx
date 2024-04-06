b87cc6cb0879a924cc77f7c63424b6fb29ea448b
Cho-Hyun-Seung <toki0327@naver.com>
2024-04-07 07:08:42 +0900

    리스트 페이지 수정

--- client/src/components/list/RegionList.tsx
+++ client/src/components/list/RegionList.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Col, Container, ListGroup, Row } from 'react-bootstrap'
+import { Col, Container, Dropdown, ListGroup, Row } from 'react-bootstrap'
 import '../../css/RegionList.css'
 
 const RegionList = () => {
@@ -8,6 +8,7 @@ const RegionList = () => {
   const [selectParentRegion, setSelectParentRegion] = useState<string>('')
   const [childRegions, setChildRegions] = useState<string[]>([])
   const [selectChildRegions, setSelectChildRegions] = useState<string[]>([])
+  const [childMenuOpen, setChildMenuOpen] = useState<boolean>(false) // 새로운 state 추가
 
   const onClickParentRegion = (regionName: string) => {
     setSelectParentRegion(regionName)
@@ -24,6 +25,13 @@ const RegionList = () => {
       })
   }
 
+  const onClickShigungu = () => {
+    if (childMenuOpen) {
+      return setChildMenuOpen(false)
+    }
+    return setChildMenuOpen(true)
+  }
+
   const onClickChildRegion = (parentRegion: string, regionName: string) => {
     const resultRegion = `${parentRegion} ${regionName}`
     if (selectChildRegions.includes(resultRegion)) {
@@ -36,15 +44,19 @@ const RegionList = () => {
   }
 
   useEffect(() => {
-    axios
-      .get(`/api/region/rootregions`)
-      .then((res: any) => {
-        const resData = res.data.map((v: any) => v['region'])
-        setRegions(resData)
-      })
-      .catch((error: any) => {
-        console.error('Error fetching root regions:', error)
-      })
+    const fetchData = async () => {
+      await axios
+        .get(`/api/region/rootregions`)
+        .then((res: any) => {
+          const resData = res.data.map((v: any) => v['region'])
+          setRegions(resData)
+        })
+        .catch((error: any) => {
+          console.error('Error fetching root regions:', error)
+        })
+    }
+
+    fetchData()
   }, [])
 
   return (
@@ -55,42 +67,85 @@ const RegionList = () => {
       </Row>
       <Row>
         <Col xs={3}>
-          <ListGroup style={{ maxHeight: '300px', overflowY: 'auto' }}>
-            {regions.map((regionName) => (
-              <ListGroup.Item
-                className={
-                  selectParentRegion === regionName
-                    ? 'parent-list-group-item active'
-                    : 'parent-list-group-item'
-                }
-                key={regionName}
-                onClick={() => onClickParentRegion(regionName)}
-              >
-                {regionName}
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
+          <Dropdown>
+            <Dropdown.Toggle
+              variant='success'
+              id='dropdown-basic'
+              className='dropdown-toggle'
+              style={{ minWidth: '168px' }}
+            >
+              {selectParentRegion === '' ? '시/도 선택' : selectParentRegion}
+            </Dropdown.Toggle>
+            <Dropdown.Menu
+              className='dropdown-menu'
+              style={{
+                overflowY: 'auto',
+                minWidth: '155px',
+                maxHeight: '200px',
+              }}
+            >
+              {regions.map((regionName) => (
+                <Dropdown.Item
+                  style={{ width: '150px' }}
+                  className={
+                    selectParentRegion === regionName
+                      ? 'parent-list-group-item active'
+                      : 'parent-list-group-item'
+                  }
+                  key={regionName}
+                  onClick={() => onClickParentRegion(regionName)}
+                >
+                  {regionName}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
         </Col>
         <Col xs={3}>
-          <ListGroup style={{ maxHeight: '300px', overflowY: 'auto' }}>
-            {childRegions.map((regionName) => (
-              <ListGroup.Item
-                className={
-                  selectChildRegions.includes(
-                    `${selectParentRegion} ${regionName}`
-                  )
-                    ? 'child-list-group-item active'
-                    : 'child-list-group-item'
-                }
-                key={regionName}
-                onClick={() =>
-                  onClickChildRegion(selectParentRegion, regionName)
-                }
-              >
-                {regionName}
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
+          <Dropdown
+            show={childMenuOpen} // 새로운 prop 추가
+          >
+            <Dropdown.Toggle
+              onClick={onClickShigungu}
+              variant='success'
+              className='dropdown-toggle'
+              style={{ minWidth: '168px', maxWidth: '168px' }}
+            >
+              {selectChildRegions.length === 0
+                ? '시/군/구 선택'
+                : selectChildRegions.length === 1
+                ? selectChildRegions[0].split(' ')[1]
+                : `${selectChildRegions[0].split(' ')[1]} 등 ${
+                    selectChildRegions.length
+                  }개`}
+            </Dropdown.Toggle>
+            <Dropdown.Menu
+              style={{
+                overflowY: 'auto',
+                minWidth: '168px',
+                maxWidth: '168px',
+                maxHeight: '200px',
+              }}
+            >
+              {childRegions.map((regionName) => (
+                <Dropdown.Item
+                  className={
+                    selectChildRegions.includes(
+                      `${selectParentRegion} ${regionName}`
+                    )
+                      ? 'child-list-group-item active'
+                      : 'child-list-group-item'
+                  }
+                  key={regionName}
+                  onClick={() =>
+                    onClickChildRegion(selectParentRegion, regionName)
+                  }
+                >
+                  {regionName}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
         </Col>
       </Row>
       <h1>{selectChildRegions}</h1>
